f2f046fca04bd3d3e883d964ea68fe858bf6c4b4
tatianenm <tatianenmlua@hotmail.com>
2025-08-02 02:26:30 -0300

    commit do projeto controle de produtos

---
+++ src/main/java/com/controledeprodutos/ControleDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.controledeprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeProdutosApplication.class, args);
+	}
+
+}
